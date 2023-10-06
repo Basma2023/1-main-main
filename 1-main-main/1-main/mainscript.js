39c153e368415fcd904d1a0ed0eeb3d74a7b01ed
Basma2023 <basma.sayyad@gmail.com>
2023-10-05 21:59:02 -0400

    This is a message

---
+++ 1-main-main/1-main/mainscript.js
@@ -0,0 +1,49 @@
+// This is a comment which can help me understand my code later
+
+/*
+This is a comment block
+it can span multiple lines
+and then it can be closed like this
+*/
+
+let x = 14;
+let y = 4;
+
+let z = x + y;
+
+// let z = x + y;
+
+const PI = 3.141591653589793;
+
+console.log(`PI: ${PI}`);
+
+let firstName = "Aisyah Putri";
+let lastName = "Megantari";
+let myFullName = `${firstName} ${lastName}`;
+
+// let myFullName = firstName + " " + lastNAme;
+
+console.log("Hello, Let Me Introduce Myself");
+console.log(`First Name: ${firstName}, Last Name: ${lastName}`);
+console.log(`My Full Name is ${myFullName}`);
+
+console.log(`X: ${x}\nZ: ${z}`);
+
+// incrementNum
+
+let incrementNum = 0;
+
+console.log(`The number: ${incrementNum}`);
+
+incrementNum++;
+
+console.log(`The incremented number: ${incrementNum}`);
+
+// interest
+let background = "interior designer";
+let home = "Indonesia";
+let thankyou = "Terimakasih";
+
+console.log(`My background is ${background}`);
+console.log(`I am from ${home}`);
+console.log(`Nice to meet you ${thankyou}`);
